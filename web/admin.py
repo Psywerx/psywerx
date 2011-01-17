fd1583b0168e4d170d8f507e05f7735006072877
Smotko <ensmotko@gmail.com>
2011-01-17 22:45:57 +0100

    Created the backend

---
+++ web/admin.py
@@ -0,0 +1,5 @@
+from web.models import Project
+from django.contrib import admin
+from django.db import transaction
+
+admin.site.register(Project)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+PROJECT_STATUS = (
+    ('D', 'Development'),
+    ('L', 'Live'),
+    ('B', 'Beta'),
+    ('A', 'Abendoned'),
+)
+
+class Project(models.Model):
+    
+    name = models.CharField(max_length = 255)
+    link = models.CharField(max_length = 255)
+    description = models.TextField()
+    status = models.CharField(max_length = 3, choices = PROJECT_STATUS)
+    
+    def __unicode__(self):
+        return self.name
