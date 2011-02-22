1b8526b026fb19ee303d8ada7e0a6a99e7e1eb80
Smotko <ensmotko@gmail.com>
2011-02-22 17:09:37 +0100

    Added Languages and Framework tables

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,7 @@
-from web.models import Project
+from web.models import Project, Framework, Language
 from django.contrib import admin
 from django.db import transaction
 
 admin.site.register(Project)
+admin.site.register(Framework)
+admin.site.register(Language)
--- web/models.py
+++ web/models.py
@@ -7,12 +7,25 @@ PROJECT_STATUS = (
     ('A', 'Abandoned'),
 )
 
-class Project(models.Model):
+class Language(models.Model):
+    name = models.CharField(max_length = 255)
     
+    def __unicode__(self):
+        return self.name
+
+class Framework(models.Model):
+    name = models.CharField(max_length= 255)
+    language = models.OneToOneField(Language)
+    
+    def __unicode__(self):
+        return self.name
+    
+class Project(models.Model):
     name = models.CharField(max_length = 255)
     link = models.CharField(max_length = 255)
     description = models.TextField()
     status = models.CharField(max_length = 3, choices = PROJECT_STATUS)
+    framework = models.ForeignKey(Framework)
     
     def __unicode__(self):
         return self.name
