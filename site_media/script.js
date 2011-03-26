9602f1f38f5d38f6535be09cc345a9dcb4ef21b3
Smotko <ensmotko@gmail.com>
2011-03-26 11:33:48 +0100

    Header text shadow changes on mouse movement.

--- site_media/script.js
+++ site_media/script.js
@@ -1,4 +1,45 @@
-$(document).ready(function(){
-	alert("Thanks for visiting!");
+jQuery(document).ready(function(){
+	
+	
+	function relativeShadow(element, color){
+		
+		
+		
+		middle = function(el){
+			m = new Object();
+			m.top = el.offset().top + el.height()/2;
+			m.left = el.offset().left +  el.width()/2;	   
+   
+			return m 
+		};
+		normalize = function(diff){
+			LIMIT = 3;
+			d = -diff/200;
+			if (d > LIMIT) d = LIMIT;
+			if (d < -LIMIT) d = -LIMIT;
+			
+			return d;			
+		}
+		
+		$(document).mousemove(function(e){
+			   
+			var p = middle(element);
+			diff = new Object();
+			diff.top = e.pageY - p.top;
+			diff.left = e.pageX - p.left;
+		
+		  
+			element.css('text-shadow', normalize(diff.left) + "px " + normalize(diff.top) +"px 0px " + color);
+		  
+		 //$('h1').html(e.pageX +', '+ e.pageY + ' ' + diff.top + ' ' + diff.left);
+		 }); 
+		
+		
+	} 
 
- });
+	r1 = new relativeShadow($("#header-h"), 'black');
+	
+   
+   
+
+});
