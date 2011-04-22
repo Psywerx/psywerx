b1cad61b48cb3793cdc8fdd88ce6031dff5b3d12
Smotko <ensmotko@gmail.com>
2011-04-22 19:36:46 +0200

    Added the Konami plugin

--- site_media/script.js
+++ site_media/script.js
@@ -1,4 +1,29 @@
+//Konami plugin:
+
+(function($) {
+
+	$.fn.konami = function(callback, code) {
+		if(code == undefined) code = "38,38,40,40,37,39,37,39,66,65";
+		
+		return this.each(function() {
+			var kkeys = [];
+			$(this).keydown(function(e){
+				kkeys.push( e.keyCode );
+				if ( kkeys.toString().indexOf( code ) >= 0 ){
+					$(this).unbind('keydown', arguments.callee);
+					callback(e);
+				}
+			}, true);
+		});
+	}
+
+})(jQuery);
+
 jQuery(document).ready(function(){
+
+
+    $(window).konami(function(){ alert('Konami Code Activated!')});
+
 	
 	
 	function relativeShadow(element, color, shadow){
