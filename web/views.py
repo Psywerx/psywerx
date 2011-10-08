0b66b86a0997b0690f5f73464a0ffa1eb186595d
Smotko <ensmotko@gmail.com>
2011-10-09 01:12:32 +0200

    Added DEBUG strings for parsing

--- web/views.py
+++ web/views.py
@@ -15,9 +15,24 @@ def index(request):
 def irc(request):
     
     if request.POST:
-        print request.POST['raw']
+        print request.POST['raw']        
+        
+        
         return HttpResponse('OK')
-    return render_to_response('irc.html', {'hello': 'world',}, context_instance=RequestContext(request))
+    DEBUG = [
+     ':smotko!~smotko@cpe-212-85-162-22.cable.telemach.net PRIVMSG #smotko-testing :hi thair!',
+     ':smotko!~smotko@cpe-212-85-162-22.cable.telemach.net PART #smotko-testing',
+     ':smotko!~smotko@cpe-212-85-162-22.cable.telemach.net JOIN #smotko-testing',
+     ':smotko!~smotko@cpe-212-85-162-22.cable.telemach.net TOPIC #smotko-testing :hello world',
+     'ERROR :Closing Link: cpe-212-85-162-22.cable.telemach.net (Ping timeout: 248 seconds)',
+     ':smotko-neki!~smotko@cpe-212-85-162-22.cable.telemach.net NICK :smotko',
+     ':smotko!~smotko@cpe-212-85-162-22.cable.telemach.net PRIVMSG botko123 :privat',
+     ':smotko!~smotko@cpe-212-85-162-22.cable.telemach.net PRIVMSG botko123 :ACTION pek'
+
+    ]
+
+    debug = DEBUG[0].split(' ', 3)
+    return render_to_response('irc.html', {'hello': debug,}, context_instance=RequestContext(request))
 
 def error500(request, template_name = '500.html'):
     return render_to_response(template_name, context_instance=RequestContext(request))
