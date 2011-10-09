c5ecff92b77af81f73b209c7ce68d1a1432b5bcc
Smotko <ensmotko@gmail.com>
2011-10-09 20:43:49 +0200

    Added tokens

--- web/views.py
+++ web/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
 from web.models import Project, Member, Irc
+import hashlib
 
 
 def index(request):
@@ -24,18 +25,21 @@ def irc_bot_add(request):
     return HttpResponse("NO")
     #return render_to_response('404.html', context_instance=RequestContext(request))
 
+MAGIC_WORD = "6cf28bcedc3a628a4896817156e1ace5108ce6266a00fd556861d656"
+COOKIE_TOKEN = "2d9aa7a812f458a8d278d35272c6dc28b03357b7db38e553ea98a7f0"
 def irc(request, page=1):
     
     # Set the cookie
     if request.POST:
-        if request.POST['word'] == 'cookie':
+        
+        if hashlib.sha224(request.POST['word']).hexdigest() == MAGIC_WORD:
             r = HttpResponseRedirect('.')
-            r.set_cookie("irctoken", "token", 60*60*24*356*100)
+            r.set_cookie("irctoken", COOKIE_TOKEN, 60*60*24*356*100)
             return r
             
     irc_token = request.COOKIES.get("irctoken", "")
     
-    if irc_token == "token":
+    if irc_token == COOKIE_TOKEN:
         q = Irc.objects.all().order_by('time').reverse()
         p = Paginator(q, 100)
         
