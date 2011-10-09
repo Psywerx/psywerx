99b173bc7d21137713c419c45352176ab1b1af0f
Smotko <ensmotko@gmail.com>
2011-10-09 17:45:59 +0200

    Enabled IRC admin page

--- web/admin.py
+++ web/admin.py
@@ -1,8 +1,9 @@
-from web.models import Project, Framework, Language, Member, Static
 from django.contrib import admin
+from web.models import Project, Framework, Language, Member, Static, Irc
 
 admin.site.register(Project)
 admin.site.register(Framework)
 admin.site.register(Language)
 admin.site.register(Member)
 admin.site.register(Static)
+admin.site.register(Irc)
