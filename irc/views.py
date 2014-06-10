4434807c31071551b73d022038b0977c7f72b2fc
Anže Pečar <ensmotko@gmail.com>
2014-06-10 12:14:22 +0200

    Show Quit messages

--- irc/views.py
+++ irc/views.py
@@ -21,7 +21,7 @@ def irc_bot_add(request):
             return HttpResponse(result)
     return HttpResponse("NO")
     #return render_to_response('404.html', context_instance=RequestContext(request))
-    
+
 @csrf_exempt
 def karma_add(request):
     if request.POST:
@@ -53,15 +53,15 @@ def karma_nick(request):
                     if o['karma'] > d[o['nick'][:4]]:
                         dn[o['nick'][:4]] = o['nick']
                     d[o['nick'][:4]] += o['karma']
-                    
+
                 import operator
                 sd = sorted(d.iteritems(), key=operator.itemgetter(1), reverse=True)
                 k = []
                 for s in sd:
                     k.append({'nick': dn[s[0]], 'karma': s[1]})
-                
+
                 karma = json.dumps(k)
-            
+
             return HttpResponse(karma)
     return HttpResponse("NO")
 
@@ -74,32 +74,33 @@ MAGIC_WORD = "6cf28bcedc3a628a4896817156e1ace5108ce6266a00fd556861d656"
 COOKIE_TOKEN = "2d9aa7a812f458a8d278d35272c6dc28b03357b7db38e553ea98a7f0"
 CHANNEL = "#psywerx"
 def irc(request, page=1, link_page=1):
-    
+
     # Set the cookie
     if request.POST:
-        
+
         if request.POST.has_key('word') and hashlib.sha224(request.POST['word']).hexdigest() == MAGIC_WORD:
             r = HttpResponseRedirect('.')
             r.set_cookie("irctoken", COOKIE_TOKEN, 60*60*24*356*100)
             return r
-            
+
     irc_token = request.COOKIES.get("irctoken", "")
-    
+
     if irc_token == COOKIE_TOKEN:
-        
+
         if request.POST.has_key('term'):
             q = Irc.objects.all().filter(message__icontains = request.POST['term'], channel__iexact = CHANNEL).order_by('time').reverse()
         else:
-            q = Irc.objects.all().filter(channel__iexact = CHANNEL).order_by('time').reverse()
-        
+            from django.db.models import Q
+            q = Irc.objects.all().filter(Q(channel__iexact = CHANNEL | msg_type = 'Q')).order_by('time').reverse()
+
         p = Paginator(q, 100)
-        
+
         links = Link.objects.all().filter(irc__channel=CHANNEL).order_by('id').reverse()
         links_p = Paginator(links, 10)
 
         # all time:
         karma = Karma.objects.all().filter(channel__iexact = CHANNEL,time__year = datetime.now().year).values('nick').annotate(karma=Count('nick')).order_by('-karma')[:5]
-        
+
         # this week:
         week_start = date.today() - timedelta(days = date.today().weekday())
         karma_week = Karma.objects.filter(time__gte = week_start, channel__iexact = CHANNEL, time__year = datetime.now().year).values('nick').annotate(karma=Count('nick')).order_by('-karma')[:5]
@@ -108,12 +109,12 @@ def irc(request, page=1, link_page=1):
         month_start = date.today() - timedelta(days = date.today().day-1)
         karma_month = Karma.objects.filter(time__gte = month_start, channel__iexact = CHANNEL, time__year = datetime.now().year).values('nick').annotate(karma=Count('nick')).order_by('-karma')[:5]
 
-                
+
         return render_to_response('irc.html', {'log': p.page(page), 'links': links_p.page(link_page), 'karma':karma, 'karma_week':karma_week, 'karma_month':karma_month}, context_instance=RequestContext(request))
     else:
         return render_to_response('irc_access.html', {'hello': "aaa",}, context_instance=RequestContext(request))
 
-@csrf_exempt   
+@csrf_exempt
 def join(request):
     if request.POST:
         if request.POST["token"] != TOKEN:
@@ -137,7 +138,7 @@ def leaveAll(request):
             return HttpResponse("NO")
         GroupMembers.leaveAll(request.POST['nick'], request.POST['channel'])
         return HttpResponse(json.dumps("ok"), mimetype="application/json")
-    
+
 @csrf_exempt
 def groups(request):
     if request.POST:
@@ -145,7 +146,7 @@ def groups(request):
             return HttpResponse("NO")
         ret = GroupMembers.groups(request.POST['channel'])
         return HttpResponse(json.dumps(', '.join(ret)), mimetype="application/json")
-    
+
 @csrf_exempt
 def mygroups(request):
     if request.POST:
@@ -153,7 +154,7 @@ def mygroups(request):
             return HttpResponse("NO")
         ret = GroupMembers.mygroups(request.POST['channel'], request.POST['nick'])
         return HttpResponse(json.dumps(', '.join(ret)), mimetype="application/json")
-    
+
 @csrf_exempt
 def mention(request):
     if request.POST:
@@ -162,5 +163,3 @@ def mention(request):
         members = GroupMembers.mention(request.POST['group'], request.POST['channel'])
         out = [(m.nick, m.channel, m.offline) for m in members];
         return HttpResponse(json.dumps(out), mimetype="application/json")
-
-
